316770c0dae4cb20917fc069be5b05d069a279e4
Manuel Enrique Pizarro Mendoza <mepizarro@soaint.com>
2024-06-17 10:15:58 -0400

    Modelos Django: -Receta y Ingrediente ahora tienen los nuevos campos y opciones de elección. -La relación muchos a muchos entre Receta e Ingrediente se mantiene. Schema GraphQL: -CreateReceta ahora incluye argumentos para los nuevos campos de Receta. -CreateIngrediente permite crear nuevos ingredientes con los campos necesarios. -Query puede resolver tanto todas las recetas como todos los ingredientes. -Mutation incluye las mutaciones para crear tanto Receta como Ingrediente.

--- recetas/models.py
+++ recetas/models.py
@@ -1,12 +1,37 @@
 from django.db import models
 
+UNIT_CHOICES = [
+    (0, "unit"),
+    (1, "gramo"),
+    (2, "ml"),
+    (3, "kilo"),
+    (4, "litro"),
+    (5, "cucharada"),
+    (6, "cucharadita"),
+    (7, "pizca"),
+]
+
+DIFFICULT_CHOICES=[
+    (1, "baja"),
+    (2, "media"),
+    (3, "alta"),
+    (4, "Master Chef"),
+]
+
+class Ingrediente(models.Model):
+    name        = models.CharField(max_length=150)
+    unit        = models.PositiveSmallIntegerField(choices=UNIT_CHOICES, default=1)
+    quantity    = models.IntegerField(default=0)#0 = A gusto Las unidades 0.5 = media,  0.25 = Cuarto, etc
+
 class Receta(models.Model):
-    name = models.CharField(max_length=150)
-    code =  models.CharField(max_length=20,blank=True, null=True)
+    name                = models.CharField(max_length=150)
+    code                = models.CharField(max_length=20,blank=True, null=True)
+    image               = models.TextField(blank=True, null=True)
+    difficulty          = models.PositiveSmallIntegerField(choices=DIFFICULT_CHOICES, default=1)
+    servings            = models.IntegerField(default=1)
+    elaboration_time    = models.IntegerField(default=10)
+    cooking_time        = models.IntegerField(default=10)
+    ingredientes = models.ManyToManyField(Ingrediente, related_name='recetas')
 
     def __str__(self):
         return self.name
-
-class Ingrediente(models.Model):
-    name = models.CharField(max_length=150)
-    unit = models.CharField(max_length=20,blank=True, null=True)
--- recetas/schema.py
+++ recetas/schema.py
@@ -14,21 +14,59 @@ class CreateReceta(graphene.Mutation):
     class Arguments:
         name = graphene.String(required=True)
         code = graphene.String()
+        image = graphene.String()
+        difficulty = graphene.Int()
+        servings = graphene.Int()
+        elaboration_time = graphene.Int()
+        cooking_time = graphene.Int()
+        ingredientes = graphene.List(graphene.Int)
 
     receta = graphene.Field(RecetaType)
 
-    def mutate(self, info, name, code=None):
-        receta = Receta(name=name, code=code)
+    def mutate(self, info, name, code=None, image=None, difficulty=1, servings=1, elaboration_time=10, cooking_time=10, ingredientes=None):
+        receta = Receta(
+            name=name,
+            code=code,
+            image=image,
+            difficulty=difficulty,
+            servings=servings,
+            elaboration_time=elaboration_time,
+            cooking_time=cooking_time
+        )
         receta.save()
+
+        if ingredientes:
+            for ingrediente_id in ingredientes:
+                ingrediente = Ingrediente.objects.get(id=ingrediente_id)
+                receta.ingredientes.add(ingrediente)
+
         return CreateReceta(receta=receta)
-    
+
+class CreateIngrediente(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        unit = graphene.Int()
+        quantity = graphene.Int()
+
+    ingrediente = graphene.Field(IngredienteType)
+
+    def mutate(self, info, name, unit=1, quantity=0):
+        ingrediente = Ingrediente(name=name, unit=unit, quantity=quantity)
+        ingrediente.save()
+        return CreateIngrediente(ingrediente=ingrediente)
+
 class Query(graphene.ObjectType):
     all_recetas = graphene.List(RecetaType)
+    all_ingredientes = graphene.List(IngredienteType)
 
     def resolve_all_recetas(self, info, **kwargs):
         return Receta.objects.all()
 
+    def resolve_all_ingredientes(self, info, **kwargs):
+        return Ingrediente.objects.all()
+
 class Mutation(graphene.ObjectType):
     create_receta = CreateReceta.Field()
+    create_ingrediente = CreateIngrediente.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)

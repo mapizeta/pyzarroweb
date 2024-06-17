5d8e73e0ea07f216eaaf5e19be48590ced8f4acc
Manuel Enrique Pizarro Mendoza <mepizarro@soaint.com>
2024-06-17 02:26:29 -0400

    allowed host

--- pyzarroweb/settings.py
+++ pyzarroweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-q+dmk-qt&)#_e5wzxq!8mx3=$=i1k0_f&)mz$81%lr17t@ri*8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.144','pyzarro.ddns.net']
 
 
 # Application definition
